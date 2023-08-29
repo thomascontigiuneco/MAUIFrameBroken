48c204a1055568210e99ea3411c680811e1172f1
thomasconti <thomas.conti@giuneco.it>
2023-08-29 16:01:00 +0200

    bug

---
+++ FrameDimensionBroken/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace FrameDimensionBroken;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+    }
+}
---
+++ FrameDimensionBroken/Platforms/Android/MainApplication.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace FrameDimensionBroken;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+        : base(handle, ownership)
+    {
+    }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
